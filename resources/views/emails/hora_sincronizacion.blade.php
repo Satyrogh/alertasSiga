c802ee8f1215c6afd57e2c76f25ad6b807fda2b5
Pablo Manríquez <pimvergara@gmail.com>
2020-08-12 14:52:28 -0400

    Email

--- resources/views/emails/hora_sincronizacion.blade.php
+++ resources/views/emails/hora_sincronizacion.blade.php
@@ -1,2 +1,6 @@
-<h1>Alerta generada por KTI Siga Vehiculos</h1>
-<h3>{{dd($hora_ultima_actualizacion)}}</h3>
+<h2>Alerta generada por KTI Siga Vehiculos</h2>
+<p>
+    <h3>Verificado a las: <b>{{$hora_verificacion}}</b></h3>
+    <h3>Última actualización: <b>{{$hora_ultima_actualizacion}}</b></h3>
+    <h3>Tiempo transcurrido desde la última verificación (Años-meses-días Horas:minutos:segundos): <b>{{$hora_diferencia}}</b></h3>
+</p>
