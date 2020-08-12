aefc4aa6ca55b4cd355cb3cf13d14d765bd01fa7
Pablo Manríquez <pimvergara@gmail.com>
2020-08-12 14:55:56 -0400

    Email config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'pmanriquez.kti@gmail.com', 'name' => 'Pablo Manríquez'],
 
     /*
     |--------------------------------------------------------------------------
